9aaa459e541a5e62e0ca740750ba012bc2b6e8a0
munyamakosa <mmunya7@gmail.com>
2022-04-03 20:16:23 +0200

    change

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
       <main>
         <Header title="Welcome to my app!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          The way you Shop is About to Change <code>and we can't wait too.</code>
         </p>
       </main>
 
