c74c33073505a165d040d47c270005b8e4ea9514
munyamakosa <mmunya7@gmail.com>
2022-04-05 12:12:01 +0200

    waiting

--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,14 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Kwingy Living in Abundance</title>
+        <title>Kwingy is Launching soon</title>
         <link rel="icon" href="/favicon.png" />
       </Head>
 
       <main>
         <Header title="Welcome to Kwingy" />
         <p className="description">
-          The way you Shop is About to Change
+          Our Team is Finalizing and will be done in the next 24hours
         </p>
 
         <iframe width="560" height="315" src="https://www.youtube.com/embed/6M6laYC0fow" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
