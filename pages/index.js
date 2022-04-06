57e5aa1c9dbf98afef913f02334c10812d3ce7ad
munyamakosa <mmunya7@gmail.com>
2022-04-06 11:16:21 +0200

    kk

--- pages/index.js
+++ pages/index.js
@@ -20,47 +20,7 @@ export default function Home() {
         <div id="paypal-button-container"></div>
       </div>
     </div>
-  <script src="https://www.paypal.com/sdk/js?client-id=ASVavUkagd5sU2AWY7sdsrLkjneoxYUUPCIpGaALhOA28j4KHp2d5s98iQORa6lapulXKrTC7pPQMLdM&enable-funding=venmo&currency=USD" data-sdk-integration-source="button-factory"></script>
-  <script>
-    function initPayPalButton() {
-      paypal.Buttons({
-        style: {
-          shape: 'pill',
-          color: 'gold',
-          layout: 'vertical',
-          label: 'checkout',
-          
-        },
-
-        createOrder: function(data, actions) {
-          return actions.order.create({
-            purchase_units: [{"description":"Web Resources FOR BRM SCHOOL","amount":{"currency_code":"USD","value":255}}]
-          });
-        },
-
-        onApprove: function(data, actions) {
-          return actions.order.capture().then(function(orderData) {
-            
-            // Full available details
-            console.log('Capture result', orderData, JSON.stringify(orderData, null, 2));
-
-            // Show a success message within this page, e.g.
-            const element = document.getElementById('paypal-button-container');
-            element.innerHTML = '';
-            element.innerHTML = '<h3>Thank you for your payment!</h3>';
-
-            // Or go to another URL:  actions.redirect('thank_you.html');
-            
-          });
-        },
-
-        onError: function(err) {
-          console.log(err);
-        }
-      }).render('#paypal-button-container');
-    }
-    initPayPalButton();
-  </script>
+  
 
         <iframe width="560" height="315" src="https://www.youtube.com/embed/6M6laYC0fow" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
       </main>
